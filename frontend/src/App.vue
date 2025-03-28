0f581929e1dd1589092a7a122b4341322fe4219e
João Gabriel <gabrieudev@gmail.com>
2025-03-27 23:13:11 -0300

    feat: importar componentes vuetify em App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import { VApp, VMain } from 'vuetify/components'
+</script>
