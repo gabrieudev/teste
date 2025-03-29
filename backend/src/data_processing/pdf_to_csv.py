ced1189f6cd0ee6102950df4e603f88e9f90dc0c
João Gabriel <gabrieudev@gmail.com>
2025-03-28 23:49:56 -0300

    refactor: otimizar scripts

--- backend/src/data_processing/pdf_to_csv.py
+++ backend/src/data_processing/pdf_to_csv.py
@@ -4,30 +4,26 @@ import zipfile
 import os
 
 def pdf_to_csv():
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    pdf_path = os.path.join(base_dir, '../../data/pdfs/anexo1.pdf')
+    csv_path = os.path.join(base_dir, '../../data/csv/dados.csv')
+    zip_path = os.path.join(base_dir, '../../data/zip/Teste_Gabriel.zip')
+
+    os.makedirs(os.path.dirname(csv_path), exist_ok=True)
+    os.makedirs(os.path.dirname(zip_path), exist_ok=True)
+
     try:
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        pdf_path = os.path.join(base_dir, '../../data/pdfs/anexo1.pdf')
-        csv_path = os.path.join(base_dir, '../../data/csv/dados.csv')
-        zip_path = os.path.join(base_dir, '../../data/zip/Teste_Gabriel.zip')
-
-        os.makedirs(os.path.dirname(csv_path), exist_ok=True)
-        with pdfplumber.open(pdf_path) as pdf, open(csv_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
-            writer = None
-            header_written = False
-
+        with pdfplumber.open(pdf_path) as pdf:
+            tables = []
             for page in pdf.pages:
-                table = page.extract_table()
-                if table:
-                    if not header_written:
-                        header = table[0]
-                        header = [col.replace('OD', 'Seg. Odontológica').replace('AMB', 'Seg. Ambulatorial') for col in header]
-                        writer = csv.writer(csvfile)
-                        writer.writerow(header)
-                        header_written = True
-                    for row in table[1:]:
-                        writer.writerow(row)
+                tables.extend(page.extract_tables())
+            with open(csv_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
+                writer = csv.writer(csvfile)
+                if tables:
+                    header = [col.replace('OD', 'Seg. Odontológica').replace('AMB', 'Seg. Ambulatorial') for col in tables[0][0]]
+                    writer.writerow(header)
+                    writer.writerows([row for table in tables for row in table[1:]])
 
-        os.makedirs(os.path.dirname(zip_path), exist_ok=True)
         with zipfile.ZipFile(zip_path, 'w') as zipf:
             zipf.write(csv_path, arcname='dados.csv')
 
@@ -38,3 +34,4 @@ def pdf_to_csv():
 
 if __name__ == "__main__":
     pdf_to_csv()
+
--- backend/src/database/download_data.py
+++ backend/src/database/download_data.py
@@ -3,35 +3,43 @@ import os
 import zipfile
 import io
 import pandas as pd
+from concurrent.futures import ThreadPoolExecutor
+
+def download_file(url, path):
+    response = requests.get(url)
+    with open(path, "wb") as f:
+        f.write(response.content)
+
+def download_and_extract_zip(url, csv_dir):
+    response = requests.get(url)
+    with zipfile.ZipFile(io.BytesIO(response.content), "r") as zipf:
+        for file in zipf.namelist():
+            if file.endswith(".csv"):
+                with zipf.open(file) as csvfile:
+                    df = pd.read_csv(csvfile, sep=";")
+                    df["VL_SALDO_INICIAL"] = df["VL_SALDO_INICIAL"].str.replace(",", ".")
+                    df["VL_SALDO_FINAL"] = df["VL_SALDO_FINAL"].str.replace(",", ".")
+                    df.to_csv(os.path.join(csv_dir, file), index=False, sep=";")
 
 def download_ans_data():
     base_url = "https://dadosabertos.ans.gov.br/FTP/PDA/"
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    
     csv_dir = os.path.join(base_dir, '../../data/csv/')
-    
     os.makedirs(csv_dir, exist_ok=True)
-    
+
     operadoras_url = base_url + "operadoras_de_plano_de_saude_ativas/Relatorio_cadop.csv"
-    response = requests.get(operadoras_url)
-    with open(os.path.join(csv_dir, "operadoras.csv"), "wb") as f:
-        f.write(response.content)
-    
-    for year in range(2023, 2025):
-        for quarter in range(1, 5):
-            zip_filename = f"{quarter}T{year}.zip"
-            demonstracoes_url = base_url + f"demonstracoes_contabeis/{year}/{zip_filename}"
-            response = requests.get(demonstracoes_url)
-            
-            with zipfile.ZipFile(io.BytesIO(response.content), "r") as zipf:
-                for file in zipf.namelist():
-                    if file.endswith(".csv"):
-                        with zipf.open(file) as csvfile:
-                            df = pd.read_csv(csvfile, sep=";")
-                            df["VL_SALDO_INICIAL"] = df["VL_SALDO_INICIAL"].str.replace(",", ".")
-                            df["VL_SALDO_FINAL"] = df["VL_SALDO_FINAL"].str.replace(",", ".")
-                            df.to_csv(os.path.join(csv_dir, file), index=False, sep=";")
-    
+    download_file(operadoras_url, os.path.join(csv_dir, "operadoras.csv"))
+
+    with ThreadPoolExecutor() as executor:
+        tasks = []
+        for year in range(2023, 2025):
+            for quarter in range(1, 5):
+                zip_filename = f"{quarter}T{year}.zip"
+                demonstracoes_url = base_url + f"demonstracoes_contabeis/{year}/{zip_filename}"
+                tasks.append(executor.submit(download_and_extract_zip, demonstracoes_url, csv_dir))
+        for task in tasks:
+            task.result()
+
     print("Arquivos baixados e extraídos com sucesso!")
 
 if __name__ == "__main__":
